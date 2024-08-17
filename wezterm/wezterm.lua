2daa4452a7641e187a07c498690fcb49c9155609
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2024-08-17 12:25:51 +0200

    :poop: Random change to test dotfile sync

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,7 +17,7 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.8
+config.window_background_opacity = 1
 config.macos_window_background_blur = 20
 
 -- and finally, return the configuration to wezterm

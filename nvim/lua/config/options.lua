d8c65f374544487cf142769d5ec6c1f1d72abe3c
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2024-12-02 23:02:29 +0100

    chore(nvim): update global nvim options; update bufferline

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.conceallevel = 0
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+vim.opt.autoindent = true
+
+vim.g.lazyvim_no_inlay_hints = true

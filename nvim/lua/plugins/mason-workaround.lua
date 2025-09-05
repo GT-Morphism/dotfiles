93c33d7068354063b190ce12662b9828cbfa90e6
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-09-05 15:15:39 +0200

    chore(nvim): remove workaround for mason

--- nvim/lua/plugins/mason-workaround.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- github issue: https://github.com/LazyVim/LazyVim/issues/6039
-  -- relevant pr: https://github.com/LazyVim/LazyVim/pull/6053
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}

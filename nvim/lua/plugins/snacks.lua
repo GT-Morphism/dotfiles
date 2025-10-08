98aade2ac22a05ae07bbd2947d28a94f5fd7c3a1
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-10-08 13:08:54 +0200

    chore(nvim): show excluded files in explorer

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -13,6 +13,13 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          include = excluded,
+        },
+      },
       hidden = true,
       ignored = true,
       exclude = excluded,
